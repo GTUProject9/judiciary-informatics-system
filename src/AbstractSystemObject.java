e58a27eac06f02ff23a3245aea2c992985b62fd7
yusufarslanpt <78217370+yusufarsln98@users.noreply.github.com>
2022-05-04 02:00:26 +0300

    Update AbstractSystemObject.java

--- src/AbstractSystemObject.java
+++ src/AbstractSystemObject.java
@@ -1,20 +1,23 @@
-
+/**
+ * Comparable System Object, it contains an if field and compareTo method.
+ */
 public abstract class AbstractSystemObject implements Comparable<AbstractSystemObject>
 {
+    // Constants
     public static final int NUMBER_OF_SYSTEM_OBJECTS = 6;
     public static final int ID_LENGTH = 5;
+    protected int id;
 
+    // Constructors
     protected AbstractSystemObject() 
     {
         id = -1;
     }
-
     protected AbstractSystemObject(int id) 
     { 
         this.id = id; 
     }
 
-    protected int id;
 
     protected int getId() {
         return id;
