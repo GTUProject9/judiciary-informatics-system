9c87230114d1da56ed3773fe098a291827cea305
samildeli <samildeliogullari@gmail.com>
2022-06-09 19:42:00 +0300

    Update LawOffice.java

--- src/LawOffice.java
+++ src/LawOffice.java
@@ -1,7 +1,6 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import enums.JobApplicationStatus;
+
+import java.util.*;
 
 public class LawOffice
 {
@@ -50,7 +49,7 @@ public class LawOffice
     }
 
     public void setName(String name) {
-        this.name = name;
+        name = name;
     }
 
     public int getOwnerId() {
@@ -58,26 +57,37 @@ public class LawOffice
     }
 
     public void setOwnerId(int ownerId) {
-        this.ownerId = ownerId;
+        ownerId = ownerId;
     }
 
-    public List<Integer> getEmployeeIds() {
-        return employeeIds;
+    public void addEmployee(int employeeId) {
+        if (!employeeIds.contains(employeeId)) {
+            employeeIds.add(employeeId);
+        }
     }
 
-    public int addEmployee(int employeeId) {
-        // Check if it already exist, if it is return null / Or change return type to boolean
-        employeeIds.add(employeeId);
-        return employeeId;
+    public int removeEmployee(int index) throws IndexOutOfBoundsException {
+        if (index >= 0 && index < employeeIds.size()) {
+            return employeeIds.remove(index);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
-    public int removeEmployee(int index) {
-        // Check if it already exist, if it is return null / Or change return type to boolean
-        return employeeIds.remove(index);
+    public int getEmployee(int index) throws IndexOutOfBoundsException {
+        if (index >= 0 && index < employeeIds.size()) {
+            return employeeIds.get(index);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
-    public List<JobAdvertisement> getJobAdvertisements() {
-        return jobAdvertisements;
+    public Iterator<Integer> getEmployeeIdsIterator() {
+        return employeeIds.iterator();
+    }
+
+    public boolean areThereEmployees() {
+        return employeeIds.size() > 0;
     }
 
     public LawOffice.JobAdvertisement createJobAdvertisement(int id, String title, String description) {
@@ -85,36 +95,57 @@ public class LawOffice
     }
     
     public void addJobAdvertisement(LawOffice.JobAdvertisement jobAdvertisement) {
-        this.jobAdvertisements.add(jobAdvertisement);
+        jobAdvertisements.add(jobAdvertisement);
     }
     
-    public LawOffice.JobAdvertisement removeJobAdvertisement(int index) {
-        return this.jobAdvertisements.remove(index);
+    public LawOffice.JobAdvertisement removeJobAdvertisement(int index)  throws IndexOutOfBoundsException {
+        if (index >= 0 && index < jobAdvertisements.size()) {
+            return jobAdvertisements.remove(index);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public Iterator<JobAdvertisement> getJobAdvertisementIterator() {
+        return jobAdvertisements.iterator();
     }
 
     public void addJobApplication(Lawyer.JobApplication jobApplication) {
-        this.jobApplications.add(jobApplication);
-    }
-
-    public List<Lawyer.JobApplication> getJobApplications() {
-        return jobApplications;
+        jobApplications.add(jobApplication);
     }
 
     public Lawyer.JobApplication getJobApplication(int index) {
-        return jobApplications.get(index);
+        if (index >= 0 && index < jobApplications.size()) {
+            return jobApplications.remove(index);
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public Iterator<Lawyer.JobApplication> getJobApplicationIterator() {
+        return jobApplications.iterator();
+    }
+
+    public boolean areThereJobApplications() {
+        return jobApplications.size() > 0;
     }
 
     public void displayEmployees(SystemClass systemClassObject)
     {
-        for (int i = 0; i < employeeIds.size(); i++)
-        {
-            System.out.println(i + ". " + systemClassObject.getLawyer(employeeIds.get(i)));
+        for (int i = 0; i < employeeIds.size(); i++) {
+            System.out.println((i + 1) + ". " + systemClassObject.getLawyer(employeeIds.get(i)));
         }
     }
 
-    public int getEmployee(int index) {
-        return employeeIds.get(index);
+    public void displayJobApplications() {
+        int i = 0;
+        for (var jobApplication : jobApplications) {
+            if (jobApplication.getStatus() == JobApplicationStatus.PENDING) {
+                System.out.println((i + 1) + ". " + jobApplication);
+            } else {
+                System.out.println("Completed job application.");
+            }
+            i++;
+        }
     }
-
-    
 }
