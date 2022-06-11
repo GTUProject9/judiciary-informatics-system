2b19ea4d27dc51b68d47b2beeef533d1d852d5f1
GTUProject9 <gtuproject09@gmail.com>
2022-06-11 16:40:28 +0300

    Hold status changed

--- src/enums/LawsuitStatus.java
+++ src/enums/LawsuitStatus.java
@@ -2,13 +2,13 @@ package enums;
 
 public enum LawsuitStatus
 {
-    HOLD_ON, STILL_GOING, SUING_WON, SUED_WON;
+    HOLD, STILL_GOING, SUING_WON, SUED_WON;
     public String getStatus()
     {
         switch(this)
         {
-            case HOLD_ON:
-                return "Hold On";
+            case HOLD:
+                return "Hold";
             case STILL_GOING:
                 return "Still Going";
             case SUING_WON:
