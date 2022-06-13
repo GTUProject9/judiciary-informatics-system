f01620dba2b39f1f22d8d445499491f137c35b51
samildeli <samildeliogullari@gmail.com>
2022-06-13 17:53:47 +0300

    Add a field for root directory path

--- src/Utils.java
+++ src/Utils.java
@@ -4,26 +4,36 @@ import java.util.Scanner;
  * It's a utility class that contains methods for reading input from the console
  */
 public final class Utils {
-    
     private Utils(){}
+
+    public static final String ROOT_DIR = getRootDirectoryPath();
 
     public static final String INVALID_INPUT = "Invalid input!";
     public static final String INVALID_CHOICE = "Invalid choice!";
 
-    
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static int readIntegerInput() throws NumberFormatException {
         String line = scanner.nextLine();
         return Integer.parseInt(line);
     }
+
     public static String readStringInput() {
         return scanner.nextLine();
     }
 
-    // Read boolean expression.
     public static boolean readBooleanInput() {
         String line = scanner.nextLine();
         return Boolean.parseBoolean(line);
+    }
+
+    private static String getRootDirectoryPath() {
+        final String workingDirectoryPath = System.getProperty("user.dir");
+        final String rootDirectoryName = "law-management-system";
+
+        int rdNameIndex = workingDirectoryPath.lastIndexOf(rootDirectoryName);
+        int rdNameLastCharIndex = rdNameIndex + rootDirectoryName.length();
+
+        return workingDirectoryPath.substring(0, rdNameLastCharIndex);
     }
 }
